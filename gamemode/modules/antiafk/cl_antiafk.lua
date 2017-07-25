6f4cd4a67595d315f6bd58b6db6557ab273bb430
Garrus2142 <garrus2142@utopia-games.net>
2017-07-26 01:00:19 +0200

    Merge addon antiafk into module

---
+++ gamemode/modules/antiafk/cl_antiafk.lua
@@ -0,0 +1,46 @@
+local scrW, scrH = ScrW(), ScrH()
+
+surface.CreateFont("Arial Big", {
+	font = "Arial",
+	size = 48
+})
+
+local function formatSeconde(sec)
+	local minutes
+	local secondes
+
+	sec = math.floor(sec)
+	minutes = math.floor(sec / 60)
+	secondes = sec % 60
+
+	if secondes <= 9 then
+		secondes = "0" .. secondes
+	end
+
+	return minutes .. ":" .. secondes
+end
+
+local alpha_bg = 0
+local function HUDPaint()
+	local text
+	if LocalPlayer():GetNWInt("afk_warn") == 0 && alpha_bg == 0 then return end
+	if LocalPlayer():GetNWInt("afk_warn") == 0 then
+		alpha_bg = Lerp(0.05, alpha_bg, 0)
+	else
+		alpha_bg = Lerp(0.05, alpha_bg, 225)
+	end
+
+	text = "Vous allez être kické du serveur dans " .. formatSeconde(LocalPlayer():GetNWInt("afk_warn") - CurTime())
+
+	surface.SetDrawColor(Color(0, 0, 0, alpha_bg))
+	surface.DrawRect(0, 0, scrW, scrH)
+
+	if LocalPlayer():GetNWInt("afk_warn") != 0 && LocalPlayer():GetNWInt("afk_warn") > CurTime() then
+		surface.SetTextColor(Color(255, 255, 255, 255))
+		surface.SetFont("Arial Big")
+		local tw, th = surface.GetTextSize(text)
+		surface.SetTextPos(scrW / 2 - tw / 2, scrH / 2 - th / 2)
+		surface.DrawText(text)
+	end
+end
+hook.Add("HUDPaint", "antiafk_HUDPaint", HUDPaint)
---
+++ gamemode/modules/antiafk/sv_antiafk.lua
@@ -0,0 +1,43 @@
+local AFK_WARN_DELAY = 60
+local AFK_KICK_DELAY = 80
+local AFK_DIST_RESET = 10000
+
+local function PlayerSpawn(ply)
+	ply.lastpos = ply:GetPos()
+	ply.lastang = ply:EyeAngles()
+	ply.afktime = CurTime()
+
+end
+hook.Add("PlayerSpawn", "antiafk_PlayerSpawn", PlayerSpawn)
+
+local function Think()
+	local curtime = CurTime()
+
+	for _, v in ipairs(player.GetAll()) do
+		if !v:Alive() || v:IsBot() then 
+			v.afktime = curtime
+			continue
+		end
+
+		-- Reset
+		if v.lastpos != v:GetPos() || v.lastang != v:EyeAngles() then
+			v.afktime = curtime
+			v.lastpos = v:GetPos()
+			v.lastang = v:EyeAngles()
+			v:SetNWInt("afk_warn", 0)
+
+		elseif curtime > v.afktime + AFK_KICK_DELAY then
+			-- Kick
+			//v:Kill("Vous avez été kick par le système d'anti AFK")
+			v:Kill()
+			v.afktime = CurTime()
+			v:SetNWInt("afk_warn", 0)
+			v:ChatPrint("[Anti-AFK] You're killed by anti-afk system !")
+
+		elseif v:GetNWInt("afk_warn") == 0 && curtime > v.afktime + AFK_WARN_DELAY then
+			-- Warning
+			v:SetNWInt("afk_warn", v.afktime + AFK_KICK_DELAY)
+		end
+	end
+end
+hook.Add("Think", "antiafk_Think", Think)
