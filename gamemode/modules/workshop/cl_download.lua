2aad0b9c310df7f2678d8d517ac75a041802a480
Guilhem PECH <guilhempech@gmail.com>
2017-07-26 18:49:54 +0200

    New module : Workshop download
    A panel will apear if the player did'nt subscribe the contents pack

---
+++ gamemode/modules/workshop/cl_download.lua
@@ -0,0 +1,9 @@
+-- Utopia Games - Slashers
+--
+-- @Author: Guilhem PECH
+-- @Date:   2017-07-26T18:34:04+02:00
+-- @Last Modified by:   Guilhem PECH
+-- @Last Modified time: 2017-07-26T18:34:05+02:00
+
+
+
---
+++ gamemode/modules/workshop/sv_download.lua
@@ -0,0 +1,9 @@
+-- Utopia Games - Slashers
+--
+-- @Author: Guilhem PECH
+-- @Date:   2017-07-26T18:35:23+02:00
+-- @Last Modified by:   Guilhem PECH
+-- @Last Modified time: 2017-07-26T18:35:23+02:00
+
+
+
