d30a1b0b9b91f376777bb26ab8520a54ac08dfc4
Guilhem PECH <guilhempech@gmail.com>
2017-07-26 17:53:56 +0200

    Fix hopefully chasemod
    Note: Garrus is a perfect bot !

--- gamemode/modules/chasemode/cl_chasemode.lua
+++ gamemode/modules/chasemode/cl_chasemode.lua
@@ -7,20 +7,21 @@
 
 
 local Timer1 = 0
-local localplayer = LocalPlayer()
+local GM = GM or GAMEMODE
 
 
 local function HaveASurvivorInSight()
+
 	local killer = team.GetPlayers(TEAM_KILLER)[1]
 	if !IsValid(killer) then return end
-	if localplayer:Team() != TEAM_KILLER then return end
+	if LocalPlayer():Team() != TEAM_KILLER then return end
 	local curtime = CurTime()
 	if Timer1 > curtime then return end
 
 	local SurvivorsPly = player.GetAll()
 	for k,v in pairs(SurvivorsPly) do
 
-		if killer:GetPos():Distance(v:GetPos()) < 1000 && localplayer:IsLineOfSightClear( v )  and v:IsValid() and v ~= localplayer  then
+		if killer:GetPos():Distance(v:GetPos()) < 1000 && LocalPlayer():IsLineOfSightClear( v )  and v:IsValid() and v ~= LocalPlayer()  then
 
 			local TargetPosMax= v:GetPos()+ v:OBBMaxs() - Vector(10,0,0)
 			local TargetPosMin = v:GetPos()+ v:OBBMins() + Vector(10,0,0)
@@ -30,9 +31,10 @@ local function HaveASurvivorInSight()
 
 
 			if (ScreenPosMax.x < ScrW() and ScreenPosMax.y < ScrH() and ScreenPosMin.x > 0 and ScreenPosMin.y > 0) then
+
 				net.Start( "sls_killerseesurvivor" )
 					net.WriteEntity( v )
-					net.WriteUInt(localplayer:GetColor().a, 8)
+					net.WriteUInt(LocalPlayer():GetColor().a, 8)
 				net.SendToServer()
 			end
 
@@ -44,17 +46,18 @@ hook.Add("Think", "sls_SurvivorInView", HaveASurvivorInSight)
 
 local ChaseSound
 local function InitValue()
-	if !IsValid(localplayer) then return end
-	localplayer = LocalPlayer()
-	ChaseSound = CreateSound( localplayer, GAMEMODE.CONFIG["chase_musics"][game.GetMap()])
-	localplayer.LastViewByKillerTime = 0
-	localplayer.ChaseSoundPlaying = false
+	if !IsValid(LocalPlayer()) then return end
+
+	ChaseSound = CreateSound( LocalPlayer(), GAMEMODE.CONFIG["chase_musics"][game.GetMap()])
+	LocalPlayer().LastViewByKillerTime = 0
+	LocalPlayer().ChaseSoundPlaying = false
 end
 hook.Add("PlayerSpawn","InitPlayValue",InitValue)
 hook.Add("sls_round_PostStart", "sls_chasemo_PostStart", InitValue)
 
 local function LastViewByKiller()
-	localplayer.LastViewByKillerTime = net.ReadFloat()
+	LocalPlayer().LastViewByKillerTime = net.ReadFloat()
+
 end
 net.Receive( "sls_chaseactivated", LastViewByKiller)
 
@@ -63,20 +66,24 @@ net.Receive( "sls_chaseactivated", LastViewByKiller)
 
 local function chaseMusic()
 	curtime= CurTime()
-	if !IsValid(ChaseSound) then return end
-	if (!localplayer:Alive() and localplayer.ChaseSoundPlaying) then ChaseSound:FadeOut(1.2) end
-	if (!localplayer:Alive()) then return end
-	if !(localplayer.LastViewByKillerTime) then return end
-	if (localplayer.LastViewByKillerTime > curtime - 3 and !localplayer.ChaseSoundPlaying) then
+
+	if (!LocalPlayer():Alive() and LocalPlayer().ChaseSoundPlaying) then ChaseSound:FadeOut(1.2) end
+
+	if (!LocalPlayer():Alive()) then return end
+	if !(LocalPlayer().LastViewByKillerTime) then return end
+
+		if (LocalPlayer().LastViewByKillerTime > curtime - 3 and !LocalPlayer().ChaseSoundPlaying) then
+
 		timer.Simple(3, function()
-			if localplayer.LastViewByKillerTime > curtime - 3 then
+			if LocalPlayer().LastViewByKillerTime > curtime - 3 then
+
 				ChaseSound:Play()
 			end
 		end)
-		localplayer.ChaseSoundPlaying = true
-	elseif localplayer.ChaseSoundPlaying and localplayer.LastViewByKillerTime < curtime - 5  then
+		LocalPlayer().ChaseSoundPlaying = true
+	elseif LocalPlayer().ChaseSoundPlaying and LocalPlayer().LastViewByKillerTime < curtime - 5  then
 		ChaseSound:FadeOut(1.2)
-		localplayer.ChaseSoundPlaying = false
+		LocalPlayer().ChaseSoundPlaying = false
 	end
 
 
--- gamemode/modules/chasemode/sv_chasemode.lua
+++ gamemode/modules/chasemode/sv_chasemode.lua
@@ -1,9 +1,9 @@
 -- Utopia Games - Slashers
 --
 -- @Author: Guilhem PECH
--- @Date:   2017-07-26T13:54:42+02:00
+-- @Date:   2017-07-26T15:18:58+02:00
 -- @Last Modified by:   Guilhem PECH
--- @Last Modified time: 2017-07-26T15:15:58+02:00
+-- @Last Modified time: 2017-07-26T17:52:15+02:00
 
 
 
@@ -16,9 +16,7 @@ local function relayChase()
 	local time = CurTime()
 	if !IsValid(GAMEMODE.ROUND.Killer) then return end
 	if color != 0 then
-
-
-
+	
 	net.Start( "sls_chaseactivated" )
 		net.WriteFloat(time)
 	net.Send(ply)
