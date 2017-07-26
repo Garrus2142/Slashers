d059007c3b9e2b9a73f3cd6ecd81b1b63469db9f
Garrus2142 <garrus2142@utopia-games.net>
2017-07-27 01:59:25 +0200

    Add convar for antiafk

--- gamemode/modules/antiafk/sv_antiafk.lua
+++ gamemode/modules/antiafk/sv_antiafk.lua
@@ -5,20 +5,22 @@
 -- @Last Modified by:   Garrus2142
 -- @Last Modified time: 2017-07-26 14:48:12
 
-local AFK_WARN_DELAY = 60
-local AFK_KICK_DELAY = 80
-local AFK_DIST_RESET = 10000
+local CV_enable = GetConVar("slashers_antiafk_enable")
+local CV_afktime = GetConVar("slashers_antiafk_afktime")
+local CV_afkmsgtime = GetConVar("slashers_antiafk_afkmsgtime")
 
 local function PlayerSpawn(ply)
 	ply.lastpos = ply:GetPos()
 	ply.lastang = ply:EyeAngles()
 	ply.afktime = CurTime()
-
 end
 hook.Add("PlayerSpawn", "antiafk_PlayerSpawn", PlayerSpawn)
 
 local function Think()
 	local curtime = CurTime()
+
+	if !CV_enable || !CV_afktime || !CV_afkmsgtime then return end
+	if !CV_enable:GetBool() then return end
 
 	for _, v in ipairs(player.GetAll()) do
 		if !v:Alive() || v:IsBot() then
@@ -33,13 +35,13 @@ local function Think()
 			v.lastang = v:EyeAngles()
 			v:SetNWInt("afk_warn", 0)
 
-		elseif curtime > v.afktime + AFK_KICK_DELAY then
+		elseif curtime > v.afktime + (CV_afktime:GetInt() + CV_afkmsgtime:GetInt()) then
 			-- Kick
 			v:Kick("You're kicked by anti-afk system.")
 
-		elseif v:GetNWInt("afk_warn") == 0 && curtime > v.afktime + AFK_WARN_DELAY then
+		elseif v:GetNWInt("afk_warn") == 0 && curtime > v.afktime + CV_afktime:GetInt() then
 			-- Warning
-			v:SetNWInt("afk_warn", v.afktime + AFK_KICK_DELAY)
+			v:SetNWInt("afk_warn", v.afktime + (CV_afktime:GetInt() + CV_afkmsgtime:GetInt()))
 		end
 	end
 end
