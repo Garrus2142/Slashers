62918d3c72412799f52c8ecd6192926f89251bec
Sodak <project-rip@web.de>
2017-08-06 17:52:50 +0200

    German Translation added

---
+++ gamemode/languages/de.lua
@@ -0,0 +1,63 @@
+
+
+
+
+local LANG = {}
+
+-- 	Haupt Spielmodus
+-- 		Überlebende
+LANG["class_desc_sports"] = "Du hast sehr viel Ausdauer und bist sehr stark. Benutze Waffen um den Killer zu betäuben und hilft den anderen wegzulaufen."
+LANG["class_desc_popular"] = "Du kennst die genaue Position der Polizei am ende des Spiels. Hilft dadurch den anderen zu entkommen."
+LANG["class_desc_nerd"] = "Benutze deinen Detektor um Spieler in der nähe zu sehen. Aber vorsicht er macht keinen Unterschied zwischen Killer und Überlebender."
+LANG["class_desc_fat"] = "Du bist der Stärkste und der am schwersten zu eliminierende Überlebende aber du hast wenig Ausdauer."
+LANG["class_desc_shy"] = "Du bist Immun gegen die erste Fähigkeit des Killers, du bist aber sehr schwach und der Killer könnte dich als oberstes Ziel auswählen."
+LANG["class_desc_junky"] = "Du hast eine erweiterte Sicht. Nutze dieses statt deiner Taschenlampe um dich besser zu verstecken."
+LANG["class_desc_emo"] = "Du bist Immun gegen die zweite Fähigkeit des Killers. Sei allein unterwegs um eine höhere Überlebenschance zu haben."
+LANG["class_desc_black"] = "Du hast 3 Schlüssel, nutze diese um Türen zu verschließen und die Überlebenden zu schützen. Wenn du eine Tür entsperrst bekommst du dein Schlüssel wieder."
+LANG["class_desc_sherif"] = "Du hast einen Revolver. Benutze ihn um den Killer zu betäuben während die anderen Überlebenden wegrennen. Du kannst den Killer nicht töten."--		Mörder
+-- Mörder
+LANG["class_desc_jason"] = "Du bist der schnellste Killer und hast eine Fähigkeit um die Fußspuren der Überlebenden zu sehen.Verfolge diese und höre genau hin, du kannst manchmal den Herzschlag der Überlebenden hören."
+LANG["class_desc_ghostface"] = "Du hast die Fähigkeit es zu sehen, wenn ein Überlebender eine Tür öffnet oder schließt. Nutze diese um einen Überlebenden schnell zu finden. Denk drüber nach geöffnete Türen wieder zu schließen um den Überlebenden zu zwingen sie erneut zu öffnen. Höre genau hin, du kannst manchmal den Herzschlag der Überlebenden hören."
+LANG["class_desc_myers"] = "Du bist der langsamste Killer und hast die Fähigkeit einen Überlebenden zur Zeit zu Verfolgen. Nutze diese Fähigkeit vorsichtig um den Überlebenden zu überraschen und schnell zu töten. Höre genau hin, du kannst manchmal den Herzschlag der Überlebenden hören."
+LANG["class_desc_proxy"] = "Du hast die Fähigkeit dich unsichtbar zu machen, wenn kein Überlebender dich sieht. Nutze dies um dich zu einem Überlebenden hinzuschleichen. Drücke deine Menü Taste um diese Fähigkeit zu nutzen (Standardmäßig 'Q'). Höre genau hin, du kannst manchmal den Herzschlag der Überlebenden hören."
+LANG["class_desc_intruder"] = "Du hast die Fähigkeit Fallen zu platzieren um die Überlebenden zu töten. Nutze die Alert Ropes um einen Überlebenden zu hören, die Bärenfalle um ihn zu töten und die Door Axe um ihn Überraschen zu töten. Höre genau hin, du kannst manchmal den Herzschlag der Überlebenden hören."
+
+LANG["round_mission_police"] = "Die Polizei kommt in %s"
+LANG["round_mission_police_killer"] = "Töte sie alle %s"
+LANG["round_mission_escape"] = "Flüchte %s"
+LANG["round_mission_escape_killer"] = "Lass sie nicht entkommen %s"
+LANG["round_mission_objectives"] = "Erfülle die Ziele %s"
+LANG["round_mission_objectives_killer"] = "Töte sie alle %s"
+LANG["round_mission_jerrycan"] = "Finde %i Benzinkanister"
+LANG["round_mission_jerrycan_found"] = "Du hast ein Benzinkanister gefunden !"
+LANG["round_mission_generator"] = "Finde und aktiviere den Generator !"
+LANG["round_mission_radio"] = "Finde und aktiviere das Radio!"
+LANG["round_wait_players"] = "Warte auf Spieler %i/%i"
+LANG["round_team_name_survivor"] = "ein Überlebender"
+LANG["round_team_name_killer"] = "der Mörder"
+LANG["round_team_desc_survivor"] = "Jeder Überlebende hat eine spezielle Fähigkeit. Euer Ziel ist es Benzinkanister (Die Anzahl variiert je nach Spieleranzahl) zu finden um den Generator zu befüllen und somit das Radio zu aktivieren um die Polizei zu rufen. Jeder Gegenstand hat mehrere Spawnpunkte die zufällig auf der Map zerstreut sind. Teamplay und der schlaue einsatz der Taschenlampe sind der Schlüssel zum Ziel."
+LANG["round_team_desc_killer"] = "Du bist ein Mörder aus einem Slasher Film und bist unsterblich. Dein Ziel ist es jeden der Überlebenden zu töten bevor sie entkommen. Du bekommst zufällig eine der folgenden Waffen: Die Axt, Die Machete oder die Kettensäge die du mit R aktivierst. Um einen starken Hieb auszuführen halte die Linke Maustaste gedrückt. Du hörst den Herzschlag der Überlebenden wenn sie sich nicht bewegen."
+LANG["round_notif_police_call"] = "Du hast die Polizei kontaktiert !"
+LANG["round_notif_police"] = "Überlebe bis die Polizei kommt !"
+LANG["round_notif_escape"] = "Flüchte"
+LANG["round_notif_escape_killer"] = "Lass sie nicht entkommen"
+LANG["round_notif_error_radio"] = "Du kannst das Radio nicht aktivieren !"
+LANG["round_notif_error_generator"] = "Du kannst den Generator nicht aktivieren !"
+LANG["round_notif_enabled_generator"] = "Du hast den Generator aktiviert !"
+LANG["round_notif_player_die"] = "%s ist gestorben"
+LANG["round_end_escaped"] = "Die Überlebenden sind geflüchtet... vorerst"
+LANG["round_end_dead"] = "Die Überlebenden wurden ausgelöscht"
+
+-- Modules
+LANG["antiafk_will_kicked"] = "Vorsicht du bist bereit wegen Inaktivität gekickt zu werden in %s"
+LANG["f1menu_you_are"] = "Du bist %s"
+LANG["workshop_need_content"] = "Du brauchst extra Content vom Workshop."
+LANG["workshop_get_it"] = "Bring mich hin!"
+LANG["workshop_no"] = "Nein lieber nicht."
+LANG["traps_rope_hit_world"] = "Das Seil hat die Welt berührt."
+LANG["traps_too_much_distance"] = "Die Entfernung ist zu groß."
+LANG["killerhelp_cant_use_ability"] = "Du kannst deine Fähigkeit gerade nicht einsetzen."
+LANG["votemap_title"] = "Stimme für die nächste Map !"
+LANG["votemap_extend"] = "Verlängern"
+LANG["votemap_random"] = "Zufällig"
+return LANG
