626fb506565187b7452f02467e07c198dbb3af14
Guilhem PECH <guilhempech@gmail.com>
2017-08-10 15:11:18 +0200

    Old variable never replaced

--- gamemode/core/rounds/sv_rounds.lua
+++ gamemode/core/rounds/sv_rounds.lua
@@ -2,8 +2,8 @@
 --
 -- @Author: Garrus2142
 -- @Date:   2017-07-25 16:15:48
--- @Last Modified by:   Garrus2142
--- @Last Modified time: 2017-08-06T10:19:06+02:00
+-- @Last Modified by:   Daryl_Winters
+-- @Last Modified time: 2017-08-10T15:08:45+02:00
 
 local GM = GM or GAMEMODE
 local MAPS_LIST
@@ -281,7 +281,7 @@ local function Think()
 	end
 
 	-- Check NextMap
-	if !GM.ROUND.Active && GM.ROUND.NextStart && curtime >= GM.ROUND.NextStart && GM.ROUND.Count >= GM.CONFIG["round_count_nextmap"] && GM.CONFIG["disabled_modules"]["votemap"] then
+	if !GM.ROUND.Active && GM.ROUND.NextStart && curtime >= GM.ROUND.NextStart && GM.ROUND.Count >= GetConVar("slashers_round_max"):GetInt() && GM.CONFIG["disabled_modules"]["votemap"] then
 		local mapindex = table.KeyFromValue(GM.MAPS, game.GetMap())
 		GM.ROUND.NextStart = nil
 		RunConsoleCommand("changelevel", mapindex == #GM.MAPS and GM.MAPS[1] or GM.MAPS[mapindex + 1])
