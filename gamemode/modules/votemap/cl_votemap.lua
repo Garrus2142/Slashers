6caef73208934a677271a2ff9e8eeedf1118ae47
Guilhem PECH <guilhempech@gmail.com>
2017-08-10 18:25:20 +0200

    Make votemap great again (again)
    Again

--- gamemode/modules/votemap/cl_votemap.lua
+++ gamemode/modules/votemap/cl_votemap.lua
@@ -2,8 +2,8 @@
 --
 -- @Author: Guilhem PECH <Daryl_Winters>
 -- @Date:   2017-08-06T09:43:46+02:00
--- @Last Modified by:   Garrus2142
--- @Last Modified time: 2017-08-07T21:05:30+02:00
+-- @Last Modified by:   Daryl_Winters
+-- @Last Modified time: 2017-08-10T18:22:57+02:00
 
 
 local GM = GAMEMODE or GM
@@ -15,8 +15,10 @@ end)
 
 
 local horizonBar
+local oneMaps = {}
 
 local function openVotemap()
+  oneMaps = {}
   backVote = vgui.Create( "DPanel" )
   backVote.isOpen = true
   local scrw = ScrW()
@@ -72,6 +74,14 @@ local function openVotemap()
     net.SendToServer()
   end
 
+  local voteCountPanel = vgui.Create("DPanel",oneMap)
+  voteCountPanel:SetTall(120)
+  voteCountPanel:SetDrawBackground(false)
+  voteCountPanel:MoveBelow(oneMap:GetChildren()[2], -30)
+  voteCountPanel:CenterHorizontal(0.5)
+
+  table.insert( oneMaps, oneMap )
+
   for k,v in pairs(slashersMaps) do
 
     if v != game.GetMap()..".bsp" then
@@ -101,7 +111,12 @@ local function openVotemap()
       end
 
       oneMap:SizeToChildren(true,true)
-
+      voteCountPanel = vgui.Create("DPanel",oneMap)
+      voteCountPanel:SetTall(120)
+      voteCountPanel:SetDrawBackground(false)
+      voteCountPanel:MoveBelow(oneMap:GetChildren()[2], -30)
+      voteCountPanel:CenterHorizontal(0.5)
+      table.insert( oneMaps, oneMap )
     end
   end
 
@@ -124,6 +139,37 @@ local function openVotemap()
     net.WriteString("random")
     net.SendToServer()
   end
+  voteCountPanel = vgui.Create("DPanel",oneMap)
+  voteCountPanel:SetTall(120)
+  voteCountPanel:SetDrawBackground(true)
+  voteCountPanel:MoveBelow(oneMap:GetChildren()[2], -30)
+
+  table.insert( oneMaps, oneMap )
+
+  for k,v in pairs(oneMaps) do
+    local votePanelCount = v:GetChild(2)
+    votePanelCount:SetWide(v:GetWide())
+    votePanelCount:AlignLeft(0)
+    local count1 = vgui.Create("DImage",votePanelCount)
+    count1:SetImage("votemap/votemap_1.png","vgui/avatar_default")
+    count1:SetKeepAspect( true )
+    count1:SetSize(120,120)
+
+
+
+    local count = vgui.Create("DImage",votePanelCount)
+    count:SetImage("votemap/votemap_1.png","vgui/avatar_default")
+    count:SetKeepAspect( true )
+    count:SetSize(120,120)
+    count:MoveRightOf(count1,0)
+
+
+    votePanelCount:SizeToChildren(true,false)
+    votePanelCount:SetDrawBackground(false)
+    count1:Hide()
+    count:Hide()
+  end
+
 
   horizonBar:SizeToChildren(true,true)
   horizonBar:MoveBelow(titleLabel,  30)
@@ -152,35 +198,34 @@ local function receiveVoteStat()
   local voteData = net.ReadTable()
   if !IsValid(backVote) then return end
   if !backVote.isOpen then return end
-  for k,v in pairs(backVote:GetChildren()[3]:GetChildren()) do
+  for k,v in pairs(oneMaps) do
 
     local nbCurVote = voteData[v:GetName()] or 0
+    local votePanelCount = v:GetChild(2)
 
 
-    if v:ChildCount() == 3 then
-      v:GetChild(2):Remove()
+    if nbCurVote == 0 then
+      votePanelCount:GetChild(1):Hide()
+      votePanelCount:GetChild(0):Hide()
     end
+
     if nbCurVote <= 5 and nbCurVote > 0 then
 
-      local count = vgui.Create("DImage",v,"vote_"..v:GetName().."_nbCurVote")
-      count:SetImage("votemap/votemap_"..nbCurVote  ..".png","vgui/avatar_default")
-      count:SetSize(120,120)
-      count:SetKeepAspect( true )
-      count:MoveBelow(v:GetChildren()[2], -30)
-      count:CenterHorizontal(0.5)
+      votePanelCount:GetChild(0):SetImage("votemap/votemap_"..nbCurVote ..".png","vgui/avatar_default")
+      votePanelCount:GetChild(0):Show()
+      votePanelCount:GetChild(0):Center()
+      votePanelCount:GetChild(1):Hide()
+
 
     elseif nbCurVote > 5 then
-      local count1 = vgui.Create("DImage",v,"vote_"..v:GetName().."_nbCurVote")
-      count1:SetImage("votemap/votemap_5.png","vgui/avatar_default")
-      count1:SetKeepAspect( true )
-      count1:SetSize(120,120)
-      count1:MoveBelow(v:GetChildren()[2],-20)
 
-      local count = vgui.Create("DImage",v,"vote_"..v:GetName().."_nbCurVote")
-      count:SetImage("votemap/votemap_"..nbCurVote - 5 ..".png","vgui/avatar_default")
-      count:SetKeepAspect( true )
-      count:SetSize(100,100)
-      count:MoveRightOf(count1,0)
+      votePanelCount:GetChild(0):SetImage("votemap/votemap_5.png","vgui/avatar_default")
+      votePanelCount:GetChild(0):AlignLeft(0)
+      votePanelCount:GetChild(0):Show()
+
+      votePanelCount:GetChild(1):SetImage("votemap/votemap_"..nbCurVote - 5 ..".png","vgui/avatar_default")
+      votePanelCount:GetChild(1):MoveRightOf(votePanelCount:GetChild(0),-20)
+      votePanelCount:GetChild(1):Show()
     end
 
     horizonBar:SizeToChildren(false,true)
