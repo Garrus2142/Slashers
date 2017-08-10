34cdea2bad372a9b307bcd073ab60c2b4b18ff98
Guilhem PECH <guilhempech@gmail.com>
2017-08-10 14:52:29 +0200

    Remove bizarre stuff - Failed merge

--- entities/entities/sls_generator/cl_init.lua
+++ entities/entities/sls_generator/cl_init.lua
@@ -2,13 +2,8 @@
 --
 -- @Author: Guilhem PECH
 -- @Date:   2017-07-26T13:54:42+02:00
-<<<<<<< HEAD
 -- @Last Modified by:   Daryl_Winters
--- @Last Modified time: 2017-08-09T17:28:04+02:00
-=======
--- @Last Modified by:   Garrus2142
--- @Last Modified time: 2017-08-09T15:07:19+02:00
->>>>>>> dbb4416ea1ad7e8b6f1f214c6c763e0a0888ca08
+-- @Last Modified time: 2017-08-10T14:50:49+02:00
 
 
 
@@ -25,11 +20,4 @@ function ENT:DrawTranslucent()
 	if LocalPlayer():IsLineOfSightClear( self.Entity ) and self.Entity:GetPos():Distance( LocalPlayer():GetPos()) < 150 and  LocalPlayer():Team() != TEAM_KILLER   then
 		DrawIndicator(self.Entity)
 	end
-<<<<<<< HEAD
-end 
-
-function ENT:Think()
-
-=======
->>>>>>> dbb4416ea1ad7e8b6f1f214c6c763e0a0888ca08
 end
